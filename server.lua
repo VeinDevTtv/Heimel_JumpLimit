accc642f4899fa6fc92b3e6d5090236dadfef462
Abdelkarim Ait Bourich <142545968+VeinDevTtv@users.noreply.github.com>
2025-04-23 06:29:41 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,10 @@
 -- server.lua
 
+local DEBUG = config.debug
+
 RegisterServerEvent('Heimel_JumpLimit:debugMessage')
-AddEventHandler('Heimel_JumpLimit:debugMessage', function(playerName, message)
-    if config.debug then
-        print(playerName .. ": " .. message)
+AddEventHandler('Heimel_JumpLimit:debugMessage', function(playerName, msg)
+    if DEBUG then
+        print(("[%s] %s"):format(playerName, msg))
     end
 end)
