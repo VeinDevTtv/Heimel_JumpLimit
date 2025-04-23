ad701d372d985d5a6329d77175d50e501742e7fc
Abdelkarim Ait Bourich <142545968+VeinDevTtv@users.noreply.github.com>
2025-04-23 06:29:28 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 config = {
-    maxJumps = 2,          -- Maximum allowed jumps before ragdoll
-    resetTime = 5000,      -- Time in milliseconds before reset (5 seconds)
-    jumpCooldown = 1000,   -- Cooldown time between jumps in milliseconds
-    ragdollTime = 3000,    -- Time in milliseconds for the ragdoll effect
-    debug = true           -- Enable or disable debug messages
+    maxJumps     = 2,    -- how many jumps you can do in a row
+    jumpCooldown = 1000, -- ms between counted jumps
+    resetTime    = 5000, -- ms after last jump before count resets
+    ragdollTime  = 3000, -- ms that ragdoll lasts
+    debug        = true, -- toggle debug messages
 }
